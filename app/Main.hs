64e1d97508a928cd2f792950867d5c9a49997c85
bigsaddev <chris.doehmann@googlemail.com>
2025-09-07 22:14:35 +0200

    bored

--- app/Main.hs
+++ app/Main.hs
@@ -20,13 +20,13 @@ g x = x**2
 
 main :: IO ()
 main = display
-  (InWindow "Vizmath" (800, 600) (50, 50))  -- window size & position
+  (InWindow "Vizmath" (800, 600) (50, 50))        -- window size & position
   black                                           -- background color
   (Pictures
-    [ drawAxes 0 0 20 globalScale -- x, y, size, scale
+    [ drawAxes 0 0 20 globalScale                 -- x, y, size, scale
     , plotFunction red f globalScale (-5) 5
     , plotFunction blue g globalScale (-5) 5
-    , Translate 50 100 $ Color red $ Scale 0.1 0.1 (Text "f(x)=3x+2")
-    , Translate 50 50 $ Color blue $ Scale 0.1 0.1 (Text "f(x)=x^2 <-- Yay Quadratische Gleichungen!")
+    , Translate 50 100 $ Color red $ Scale 0.1 0.1 $ Text "f(x)=3x+2"
+    , Translate 50 50 $ Color blue $ Scale 0.1 0.1 $ Text "f(x)=x^2 <-- Yay Quadratische Gleichungen!"
     ]
   )
